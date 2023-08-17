ddcbd74c3650ac193631e72db2443e830e268c92
Cluck <cluck@Clucks-MacBook-Pro.local>
2023-08-17 09:37:22 +0700

    Add delegate check cart

--- Sources/CartActionButton/CartActionButton.swift
+++ Sources/CartActionButton/CartActionButton.swift
@@ -11,6 +11,13 @@ import UIKit
 public protocol CartActionButtonDelegate: AnyObject {
     func cartActionButtonShouldExpandButton(_ cart: CartActionButton) -> Bool
     func cartActionButton(_ cart: CartActionButton, didChange quantity: CartActionButton.QuantityChange)
+    func checkCartOver100() -> Bool
+}
+
+public extension CartActionButtonDelegate {
+    func checkCartOver100() -> Bool {
+        false
+    }
 }
 
 // MARK: -
@@ -281,6 +288,7 @@ private extension CartActionButton {
 
     @objc func plusButtonAction(_ sender: UIButton) {
         guard delegate?.cartActionButtonShouldExpandButton(self) == true else { return }
+        guard delegate?.checkCartOver100() == false else { return }
         if isExpanded == false {
             expandButton(true)
         }
